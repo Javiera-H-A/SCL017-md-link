bda2d6a6d212a6b5060d4aecea07f8f9b2c1f0d6
Javiera Herrera <javierab.herrera@gmail.com>
2021-09-05 23:28:15 -0300

    Modificando el package

--- index.js
+++ index.js
@@ -1,151 +1,125 @@
  
 const fs=require('fs');
 const PATH=require('path');
-// const https = require('https')
-// const request = require('sync-request');
-// const axios = require('axios');
 const fetch= require('fetch')
 
 
 
-function MdLinks(path,options={validate: false}) {
-  let currentPath = PATH.basename(__dirname)
-  if(fs.lstatSync(path).isDirectory()){ 
-  let fileList = fs.readdirSync(path);
+function MdLinks(path,options={validate: false, stats: false}) {
+    let currentPath = PATH.basename(__dirname)
+    if(fs.lstatSync(path).isDirectory()){ 
+        let fileList = fs.readdirSync(path);
 
-for( let j=0; j<fileList.length; j++){
-  let filePath = PATH.join(path,fileList[j]);
-  let relativePath = PATH.relative(currentPath,filePath)
-  let readFile = fs.readFileSync(filePath, (error,datos) => {
-    if (error)
-      console.log(error);
-    else
-      return datos;
-    });
+        for( let j=0; j<fileList.length; j++){
+            let filePath = PATH.join(path,fileList[j]);
+            let relativePath = PATH.relative(currentPath,filePath)
+            let readFile = fs.readFileSync(filePath, (error,datos) => {
+                if (error){
+                    console.log(error)
+                }
+                else{
+                    return datos
+                }
+            });
 
-  let mdContents = readFile.toString();
-  // console.log(mdContents.slice(0,200))
-  const regexMdLinks = /\[([^\[]+)\](\(.*\))/gm
-  const matches = mdContents.match(regexMdLinks)
-  const singleMatch = /\[([^\[]+)\]\((.*)\)/ 
-   
-for(let i = 0; i < matches.length; i++) {
-  let text = singleMatch.exec(matches[i])
-  // console.log(text[2])
-  if (options.validate){
-      // axios.get(text[2])
-      // .then((response) => {
-      //   console.log(`Text: ${text[1].slice(0,50)}, Link: ${text[2]}`+ ' File: ' + relativePath+ ' Status: '+ response.status+' Ok: '+response.statusText + '\n')        
-      // }, (error) => {  
-      //   console.log(error.response)       
-      // } );
-      fetch.fetchUrl(text[2], function(error, meta, body){
-        if (body === undefined){
+            let mdContents = readFile.toString();
+            const regexMdLinks = /\[([^\[]+)\](\(.*\))/gm
+            const matches = mdContents.match(regexMdLinks);
+            const singleMatch = /\[([^\[]+)\]\((.*)\)/ 
+            let listLinks = [];
 
-        }else{
-          if(meta.status>= 200 && meta.status<400){
-            console.log(`Text: ${text[1].slice(0,50)}, Link: ${text[2]}`+ ' File: ' + relativePath + ' Status: '+ meta.status+' Ok: '+'ok' + '\n')
-          }else{
-            console.log(`Text: ${text[1].slice(0,50)}, Link: ${text[2]}`+ ' File: ' + relativePath + ' Status: '+ meta.status+' Ok: '+'fail' + '\n')
-            
-          }
-        }
-    });
-
-
-  }else{
-    console.log(`Text: ${text[1].slice(0,50)}, Link: ${text[2]}`+ ' File: ' + relativePath + '\n')
-
-  }
-      
-
-
-
-  // const options = {
-  //   hostname: 'nodesource.com',
-  //   path: '/products/nsolid',
-  //   // port: 443, 
-  //   method: 'GET'
-  // }
-
-  // const req = https.got(options, res => {
-  //   // console.log(`statusCode: ${res.statusCode}`)
-  //   console.log(`#${i} Text: ${text[1].slice(0,50)}, Link: ${text[2]}`+ ' File: ' + relativePath+ '\\' +files[j] )
-
+            for(let i = 0; i < matches.length; i++) {
+                let text = singleMatch.exec(matches[i]);
+                listLinks.push(text[2]);
+                
+                if(!options.stats){
+                
+                if (options.validate){
+                    fetch.fetchUrl(text[2], function(error, meta, body){
+                        if (body === undefined){
+                        }
+                        else{
+                            if(meta.status>= 200 && meta.status<400){
+                                console.log(`Text: ${text[1].slice(0,50)}, Link: ${text[2]}`+ ' File: ' + relativePath + ' Status: '+ meta.status+' Ok: '+'ok' + '\n')
+                            }
+                            else{
+                                console.log(`Text: ${text[1].slice(0,50)}, Link: ${text[2]}`+ ' File: ' + relativePath + ' Status: '+ meta.status+' Ok: '+'fail' + '\n')
+                            }
+                        }
+                    });
+                }
+                else{
+                    console.log(`Text: ${text[1].slice(0,50)}, Link: ${text[2]}`+ ' File: ' + relativePath + '\n')                    
+                }
+            }}
+            if(options.stats){
+                console.log(fileList[j])
+                console.log("Total", listLinks.length);
+                let unique = new Set(listLinks);
+                console.log("Unique", [...unique].length);                
+            }        
+        }        
+    }
     
-  // })
-  // req.on('error', error => {
-  //   console.error(error)
-  // })
-  // req.end()
-  // let res = request('GET', text[2]) 
-  // console.log(res.statusCode);
+    else{
+        let filePath = path;
+        let relativePath = PATH.relative(currentPath,filePath);
+        let readFile = fs.readFileSync(path, (error,datos) => {
+            if (error){
+            console.log(error)
+            }
+            else{
+                return datos;
+            }
+        });
 
-  // if (error) { 
-  //     console.log(error);
-  // }
-  //   else
-  //     return datos;
-  
-    
+        let mdContents = readFile.toString();
+        const regexMdLinks = /\[([^\[]+)\](\(.*\))/gm
+        const matches = mdContents.match(regexMdLinks)
+        const singleMatch = /\[([^\[]+)\]\((.*)\)/ 
+        let listLinks = [];
 
-  
- 
+        for(let i = 0; i < matches.length; i++) {
+            let text = singleMatch.exec(matches[i])
+            listLinks.push(text[2]);
 
+            if(!options.stats){
+
+            if (options.validate){
+                fetch.fetchUrl(text[2], function(error, meta, body){
+                    if (body === undefined){
+                    }
+                    else{
+                        if(meta.status>= 200 && meta.status<400){
+                            console.log(`Text: ${text[1].slice(0,50)}, Link: ${text[2]}`+ ' File: ' + relativePath + ' Status: '+ meta.status+' Ok: '+'ok' + '\n')
+                        }
+                        else{
+                            console.log(`Text: ${text[1].slice(0,50)}, Link: ${text[2]}`+ ' File: ' + relativePath + ' Status: '+ meta.status+' Ok: '+'fail' + '\n')
+                        }
+                    }
+                });
+            }
+            else{
+                console.log(`Text: ${text[1].slice(0,50)}, Link: ${text[2]}`+ ' File: ' + relativePath + '\n')                
+            }           
+        }        
+    }
+    if(options.stats){
+        console.log(filePath)
+        console.log("Total", listLinks.length);
+        let unique = new Set(listLinks);
+        console.log("Unique", [...unique].length);
+        
+    }}        
 }
-}}else{
-  let filePath = path;
-  let relativePath = PATH.relative(currentPath,filePath);
-  let readFile = fs.readFileSync(path, (error,datos) => {
-    if (error)
-      console.log(error);
-    else
-      return datos;
-    });
-
-  let mdContents = readFile.toString();
-  // console.log(mdContents.slice(0,200))
-  const regexMdLinks = /\[([^\[]+)\](\(.*\))/gm
-  const matches = mdContents.match(regexMdLinks)
-  const singleMatch = /\[([^\[]+)\]\((.*)\)/ 
-   
-for(let i = 0; i < matches.length; i++) {
-  let text = singleMatch.exec(matches[i])
-  // console.log(text[2])
-  if (options.validate){
-      // axios.get(text[2])
-      // .then((response) => {
-      //   console.log(`Text: ${text[1].slice(0,50)}, Link: ${text[2]}`+ ' File: ' + relativePath+ ' Status: '+ response.status+' Ok: '+response.statusText + '\n')        
-      // }, (error) => {  
-      //   console.log(error.response)       
-      // } );
-      fetch.fetchUrl(text[2], function(error, meta, body){
-        if (body === undefined){
-
-        }else{
-          if(meta.status>= 200 && meta.status<400){
-            console.log(`Text: ${text[1].slice(0,50)}, Link: ${text[2]}`+ ' File: ' + relativePath + ' Status: '+ meta.status+' Ok: '+'ok' + '\n')
-          }else{
-            console.log(`Text: ${text[1].slice(0,50)}, Link: ${text[2]}`+ ' File: ' + relativePath + ' Status: '+ meta.status+' Ok: '+'fail' + '\n')
-
-          }
-        }
-    });
-
-
-  }else{
-    console.log(`Text: ${text[1].slice(0,50)}, Link: ${text[2]}`+ ' File: ' + relativePath + '\n')
-
-  }}
-}
-}
-
-
+exports.MdLinks=MdLinks;
 
 // MdLinks('C:/Users/javie/Desktop/proyectos/Cuarto proyecto/SCL017-md-link/Markdown/',option = {validate: true})
 // MdLinks('C:/Users/javie/Desktop/proyectos/Cuarto proyecto/SCL017-md-link/Markdown/',option = {validate: false})
 // MdLinks('C:/Users/javie/Desktop/proyectos/Cuarto proyecto/SCL017-md-link/Markdown/README2.md',option = {validate: true})
 // MdLinks('C:/Users/javie/Desktop/proyectos/Cuarto proyecto/SCL017-md-link/Markdown/README2.md',option = {validate: false})
-MdLinks('C:/Users/javie/Desktop/proyectos/Cuarto proyecto/SCL017-md-link/Markdown/', {validate:true})
+// MdLinks('C:/Users/javie/Desktop/proyectos/Cuarto proyecto/SCL017-md-link/Markdown/README2.md',{stats:true})
+// MdLinks('C:/Users/javie/Desktop/proyectos/Cuarto proyecto/SCL017-md-link/Markdown/')
+
 
 
